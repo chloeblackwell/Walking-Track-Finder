822afb889e4a63ed167c7b4aa1d4979e29114e34
Chloe Blackwell <chloeblackwell2@hotmail.com>
2021-02-09 19:51:21 +1100

    geocoder added

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import mapboxgl, { MapboxGeocoder } from 'mapbox-gl';
-import Geocoder from 'react-mapbox-gl-geocoder';
+import mapboxgl from 'mapbox-gl';
 import "./map.css";
-
+import MapboxGeocoder from "mapbox-gl-geocoder"
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -26,8 +24,13 @@ class DisplayMap extends React.Component {
             zoom: this.state.zoom
         });
 
-        var geocoder = new Geocoder({ mapboxApiAccessToken: mapboxgl.accessToken });
-        map.addControl(geocoder);
+        map.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+
+            })
+        );
 
         map.on('move', () => {
             this.setState({
@@ -37,18 +40,6 @@ class DisplayMap extends React.Component {
             });
         });
 
-        // var geocoder = new Geocoder({ // Initialize the geocoder
-        //     mapboxApiAccessToken: mapboxgl.accessToken, // Set the access token
-        //     mapboxgl: mapboxgl, // Set the mapbox-gl instance
-        //     marker: false, // Do not use the default marker style
-        // });
-
-        // // Add the geocoder to the map
-        // map.addControl(geocoder);
-
-
-
-
     }
 
     render() {
@@ -57,7 +48,7 @@ class DisplayMap extends React.Component {
                 <div>
                     <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
                 </div>
-                {/* <div position="top-left">{this.geocoder}</div> */}
+
                 <div ref={el => this.mapContainer = el} className='mapContainer' />
             </div>
         )
